0baf75a04d84523ce677dfc1bae807c8de8be5ac
SteveNyamari <124403899+SteveNyamari@users.noreply.github.com>
2023-03-11 12:16:14 +0300

    Add files via upload
    A program that dispays "Hello World" in a message dialogue box

---
+++ Helloworld.java
@@ -0,0 +1,19 @@
+/*
+ This Progrm displays hello world in a Message Dialog  box.
+ */
+package helloworld;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author STEPHEN
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World" );
+    }   
+}
